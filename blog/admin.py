6640174548d99fbd0682a66587ab89f91f489201
Kat632 <katherine.biggs@mail.com>
2022-04-11 18:18:15 +0000

    add recipe model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Use Summernote for recipe admin
+    """
+    summernote_fields = ('content')

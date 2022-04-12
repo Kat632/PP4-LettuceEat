bc8cd6f5160c8f6c5e528be615aac8f7a0c11c1d
Kat632 <katherine.biggs@mail.com>
2022-04-12 08:39:53 +0000

    add comment model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
@@ -7,4 +7,25 @@ class RecipeAdmin(SummernoteModelAdmin):
     """
     Use Summernote for recipe admin
     """
-    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('method')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Ability to manage comments in admin panel
+    """
+    list_display = ('name', 'body', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        """
+        approval of comments
+        """
+        queryset.update(approved=True)
